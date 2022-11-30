fd5058a099283e9c58d4f27983ad7e98cb3db0c4
Lindsay Nelson <lindsaynelson@Lindsays-MacBook-Pro-7.local>
2022-11-30 11:49:37 -0500

    try1

--- discussion_12.py
+++ discussion_12.py
@@ -16,6 +16,8 @@ def setUpDatabase(db_name):
 # TASK 1
 # CREATE TABLE FOR EMPLOYEE INFORMATION IN DATABASE AND ADD INFORMATION
 def create_employee_table(cur, conn):
+    cur.execute('CREATE TABLE IF NOT EXISTS Employees (employee_id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, job_id INTEGER, hire_date TEXT, salary INTEGER')
+    conn.commit()
     pass
 
 # ADD EMPLOYEE'S INFORMTION TO THE TABLE
@@ -27,15 +29,26 @@ def add_employee(filename, cur, conn):
     file_data = f.read()
     f.close()
     # THE REST IS UP TO YOU
+    json_data = json.loads(file_data)
+    for person in json_data:
+        cur.execute('INSERT INTO employees (employee_id, first_name, last_name, job_id, hire_date, salary) VALUES (?, ?, ?, ?, ?, ?)', (person["employee_id"], person["first_name"], person["last_name"], person["job_id"], person["hire_date"], person["salary"]))
+    conn.commit()
     pass
 
 # TASK 2: GET JOB AND HIRE_DATE INFORMATION
 def job_and_hire_date(cur, conn):
+    cur.execute("SELECT employees.hire_date, jobs.job_title FROM employees JOIN jobs ON jobs.job_id = employees.job_id")
+    results = cur.fetchall()
+    job = sorted(results, key = lambda x: x[0])
+    return job[0][1]
     pass
 
 # TASK 3: IDENTIFY PROBLEMATIC SALARY DATA
 # Apply JOIN clause to match individual employees
 def problematic_salary(cur, conn):
+    cur.execute("SELECT employees.first_name, employees.last_name FROM employees JOIN jobs ON jobs.job_id = employees.job_id WHERE employees.salary < jobs.min_salary OR employees.salary > jobs.max_salary")
+    results = cur.fetchall()
+    return results 
     pass
 
 # TASK 4: VISUALIZATION
